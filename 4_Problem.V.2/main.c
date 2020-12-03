54740f903ab986ddc2b5e3e6ec2b559ff18b9a54
Minerkow <melnikov.ignat@phystech.edu>
2020-12-03 17:48:25 +0300

    add critical section markup

--- 4_Problem.V.2/main.c
+++ 4_Problem.V.2/main.c
@@ -59,6 +59,7 @@ int main(int argc, char ** argv){
 //        exit(EXIT_FAILURE);
 //    }
 
+/*(5)Begin Child-Parent for parent's handlers[62-193]*/
     pid = fork();
     if (pid < 0) {
         perror("fork");
@@ -67,6 +68,7 @@ int main(int argc, char ** argv){
 
     if (pid == 0) {
 
+/*(2)Begin Child-Core for prctl bucket[71-81]*/
         if (prctl(PR_SET_PDEATHSIG, SIGKILL) < 0) {
             perror("prctl()");
             exit(EXIT_FAILURE);
@@ -76,6 +78,7 @@ int main(int argc, char ** argv){
             fprintf(stderr, "Daddy DEAD!\n");
             exit(EXIT_FAILURE);
         }
+/*(2)End Child-Core for prctl bucket*/
 
         if (sigfillset(&set) < 0) {
             perror("sigemptyset");
@@ -112,6 +115,8 @@ int main(int argc, char ** argv){
                 break;
             }
             for (int i = 128; i >= 1; i /= 2){
+/*(1*)Begin Child-Parent for parent's PMS(in Child) [118-133] + parent handler*/
+/*(5*)End Child-Parent for parent's handlers[119-131]*/
                 if (i & sym ) {
                     if (kill(ppid, SIGUSR1) < 0) {
                         perror("kill");
@@ -123,8 +128,9 @@ int main(int argc, char ** argv){
                         exit(EXIT_FAILURE);
                     }
                 }
-
+/*(5*)End Child-Parent for parent's handlers*/
                 sigsuspend(&set);
+/*(1*)End Child-Parent for parent's PMS(in Child)*/
             }
         }
         fprintf(stderr, "I want DEI\n");
@@ -174,14 +180,17 @@ int main(int argc, char ** argv){
     sigdelset(&set, SIGCHLD);
 
     while(1) {
+/*(4)Begin for numbit in Parent[183-190] + Parent's Handler*/
         if(numBit == 0){
             printf("%c", rcvSym);
             fflush(0);
             numBit=128;
             rcvSym = 0;
         }
-
+/*(4)End for numbit in Parent*/
+/*(3)Parent-Child for child's PMS[192-191](in Parent) + child's handler*/
         sigsuspend(&set);
+/*(5)End Child-Parent for parent's handlers*/
         if (kill(pid, SIGUSR1) < 0) {
             perror("kill");
             exit(EXIT_FAILURE);
@@ -196,11 +205,14 @@ void Child_Dead_Handler(int signo) {
     fprintf(stderr, "Child dead!\n");
     exit(EXIT_SUCCESS);
 }
-
+/*Begin child's handler*/
 void Confirm_Handler(int signo) {
     //fprintf(stderr, "Confirm\n");
 }
+/*End child's handler*/
 
+/*(1)Begin Child-Parent for pending mask signals [214-227](in Parent) + Parent's handlers*/
+/*Begin Parent's handlers*/
 void One_Handler(int signo) {
     rcvSym += numBit;
     numBit /= 2;
@@ -211,6 +223,9 @@ void Zero_Handler(int signo) {
     numBit/=2;
     //fprintf(stderr, "rcv0\n");
 }
+/*End Parent's handlers*/
+/*(1)End Child-Parent for parent's PMS(in Parent)*/
+
 
 //void Daddy_Dead_Handler(int signo) {
 //    fprintf(stderr, "Daddy DEAD!");
